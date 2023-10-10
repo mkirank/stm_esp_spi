28b578a6d024fbb97a5426280b871d883e058f95
kiran <kiran@brainturk.com>
2023-10-10 15:25:12 +0530

    indentation

--- esp_slave/esp_slave.ino
+++ esp_slave/esp_slave.ino
@@ -1,48 +1,43 @@
 #include <ESP32SPISlave.h>
 
-
 ESP32SPISlave slave;
 
-
-static constexpr uint32_t BUFFER_SIZE {1};
+static constexpr uint32_t BUFFER_SIZE{1};
 uint8_t spi_slave_tx_buf[BUFFER_SIZE];
 uint8_t spi_slave_rx_buf[BUFFER_SIZE];
 uint8_t data;
 
+void setup()
+{
+    Serial.begin(9600);
+    delay(2000);
+    slave.setDataMode(SPI_MODE0);
+    slave.begin(VSPI); // VSPI is not the default for slave mode
 
-void setup() {
-Serial.begin(9600);
-delay(2000);
-slave.setDataMode(SPI_MODE0);
-slave.begin(VSPI); // VSPI is not the default for slave mode
-
-
-// clear buffers
-memset(spi_slave_tx_buf, 0, BUFFER_SIZE);
-memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
+    // clear buffers
+    memset(spi_slave_tx_buf, 0, BUFFER_SIZE);
+    memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
 }
 
+void loop()
+{
+    spi_slave_tx_buf[0] = data;
 
-void loop() {
-spi_slave_tx_buf[0] = data;
+    // block until the transaction comes from master
+    slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
 
-
-// block until the transaction comes from master
-slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
-
-
-// if transaction has completed from master,
-// available() returns size of results of transaction,
-// and buffer is automatically updated
-while (slave.available()) {
-Serial.print("Sent to master: ");
-Serial.print(spi_slave_tx_buf[0]);
-Serial.print(", Received from master: ");
-Serial.println(spi_slave_rx_buf[0]);
-data = spi_slave_rx_buf[0];
-slave.pop();
+    // if transaction has completed from master,
+    // available() returns size of results of transaction,
+    // and buffer is automatically updated
+    while (slave.available())
+    {
+        Serial.print("Sent to master: ");
+        Serial.print(spi_slave_tx_buf[0]);
+        Serial.print(", Received from master: ");
+        Serial.println(spi_slave_rx_buf[0]);
+        data = spi_slave_rx_buf[0];
+        slave.pop();
+    }
+    Serial.println(". ");
+    delay(1000);
 }
-Serial.println(". ");
-delay(1000);
-}
-
--- stm_master/stm_master.ino
+++ stm_master/stm_master.ino
@@ -1,43 +1,35 @@
 #include <SPI.h>
 
-
 const int slaveSelectPin = PA4; // SS (Slave Select) pin
 byte dataToSend = 0x3a;
 
+void setup()
+{
+    Serial.begin(9600);
 
-void setup() {
-Serial.begin(9600);
+    pinMode(slaveSelectPin, OUTPUT);
+    digitalWrite(slaveSelectPin, HIGH); // Deselect the slave device initially
 
-
-pinMode(slaveSelectPin, OUTPUT);
-digitalWrite(slaveSelectPin, HIGH); // Deselect the slave device initially
-
-
-SPI.begin();
+    SPI.begin();
 }
-void loop() {
-Serial.print('x');
-SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+void loop()
+{
+    Serial.print('x');
+    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
 
+    digitalWrite(slaveSelectPin, LOW); // Select the slave device
 
-digitalWrite(slaveSelectPin, LOW); // Select the slave device
+    byte dataReceived = SPI.transfer(dataToSend);
 
+    digitalWrite(slaveSelectPin, HIGH); // Deselect the slave device
+    SPI.endTransaction();
 
-byte dataReceived = SPI.transfer(dataToSend);
+    Serial.print("Sent to slave: ");
+    Serial.print(dataToSend);
+    Serial.print(", Received from slave: ");
+    Serial.println(dataReceived);
 
+    dataToSend = dataReceived + 1;
 
-digitalWrite(slaveSelectPin, HIGH); // Deselect the slave device
-SPI.endTransaction();
-
-
-Serial.print("Sent to slave: ");
-Serial.print(dataToSend);
-Serial.print(", Received from slave: ");
-Serial.println(dataReceived);
-
-
-dataToSend = dataReceived + 1;
-
-
-delay(1000);
+    delay(1000);
 }
