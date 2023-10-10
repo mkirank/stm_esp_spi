47eae62611208e10e87faa2233ec922304cc95a5
kiran <kiran@brainturk.com>
2023-10-10 13:39:39 +0530

    vspi

--- esp_slave/esp_slave.ino
+++ esp_slave/esp_slave.ino
@@ -1,41 +1,32 @@
-#include <SPI.h>
+#include <ESP32SPISlave.h>
 
-const int slaveSelectPin = 5;  // VSPI_CSO (Slave Select) pin
+SPIClass * vspi = NULL;
+ESP32SPISlave slave(VSPI_HOST, MISO, MOSI, SCLK, CS); // Define SPI pins
 
 void setup() {
-    //pinMode(slaveSelectPin, INPUT);
-    pinMode(slaveSelectPin, INPUT_PULLUP);
-    Serial.begin(9600);
-    SPI.begin();  // Use default VSPI pins: SCK=18, MISO=19, MOSI=23, SS=5
+    Serial.begin(115200);
+    Serial.println("ESP32 SPI Slave");
+
+    // Initialize the SPI slave interface
+    slave.begin();
+
+    // Set the callback when data is received
+    slave.setDataReceivedCallback(onDataReceived);
 }
 
 void loop() {
-    if (digitalRead(slaveSelectPin) == LOW) {
-        Serial.println("Slave Selected!");
-    } else {
-      Serial.print("X");
-    }
-    delay(500);
+    // Continuously poll the slave
+    slave.poll();
+    delay(10);
 }
 
-void loop1() {
-    if (digitalRead(slaveSelectPin) == LOW) {
-        SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
-        
-        byte dataToSend = 0x00;  // Dummy byte
-        byte dataReceived = SPI.transfer(dataToSend);
-
-        Serial.print("Sent to master: ");
-        Serial.print(dataToSend);
-        Serial.print(", Received from master: ");
-        Serial.println(dataReceived);
-
-        byte response = 0x3a;  // Response byte
-        SPI.transfer(response);
-
-        SPI.endTransaction();
+void onDataReceived(uint8_t * data, size_t len) {
+    // Print received data
+    Serial.print("Received: ");
+    for (size_t i = 0; i < len; i++) {
+        Serial.print(data[i], HEX);
+        Serial.print(" ");
     }
-    Serial.print(". ");
-    delay(1000);
+    Serial.println();
 }
 
