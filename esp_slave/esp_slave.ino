bd8a3046f54dc42027a86436dfa748eeed71d3a9
kiran <kiran@brainturk.com>
2023-10-10 13:58:59 +0530

    lib error

--- esp_slave/esp_slave.ino
+++ esp_slave/esp_slave.ino
@@ -1,37 +1,44 @@
 #include <ESP32SPISlave.h>
 
-
 ESP32SPISlave slave;
 
+static constexpr uint32_t BUFFER_SIZE {32};
+uint8_t spi_slave_tx_buf[BUFFER_SIZE];
+uint8_t spi_slave_rx_buf[BUFFER_SIZE];
 
 void setup() {
-Serial.begin(9600);
-Serial.println("ESP32 SPI Slave");
+    Serial.begin(115200);
+    delay(2000);
 
+    // begin() after setting
+    // HSPI = CS: 15, CLK: 14, MOSI: 13, MISO: 12 -> default
+    // VSPI = CS:  5, CLK: 18, MOSI: 23, MISO: 19
+    slave.setDataMode(SPI_MODE0);
+    slave.begin();
+    // slave.begin(VSPI);   // you can use VSPI like this
 
-// Initialize the SPI slave interface
-slave.setDataMode(SPI_MODE0);
-slave.begin(VSPI);
-
-
-// Set the callback when data is received
-slave.setDataReceivedCallback(onDataReceived);
+    // clear buffers
+    memset(spi_slave_tx_buf, 0, BUFFER_SIZE);
+    memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
 }
-
 
 void loop() {
-// Continuously poll the slave
-slave.poll();
-delay(10);
+    // block until the transaction comes from master
+    slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
+
+    // if transaction has completed from master,
+    // available() returns size of results of transaction,
+    // and buffer is automatically updated
+    while (slave.available()) {
+        // show received data
+        Serial.print("Command Received: ");
+        Serial.println(spi_slave_rx_buf[0]);
+        slave.pop();
+    }
+
+    // Additional delay or processing if needed
+    // ...
+
+    delay(1000);  // Delay for a second to easily read the serial data
 }
 
-
-void onDataReceived(uint8_t * data, size_t len) {
-// Print received data
-Serial.print("Received: ");
-for (size_t i = 0; i < len; i++) {
-Serial.print(data[i], HEX);
-Serial.print(" ");
-}
-Serial.println();
-}
