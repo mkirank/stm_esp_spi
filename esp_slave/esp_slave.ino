9795bdeb7ea4ba99db7469f941437cecfc351ef6
kiran <kiran@brainturk.com>
2023-10-10 14:02:33 +0530

    chatgpt

--- esp_slave/esp_slave.ino
+++ esp_slave/esp_slave.ino
@@ -1,6 +1,6 @@
 #include <ESP32SPISlave.h>
 
-ESP32SPISlave slave;
+ESP32SPISlave slave(VSPI_HOST);  // Use the VSPI host
 
 static constexpr uint32_t BUFFER_SIZE {32};
 uint8_t spi_slave_tx_buf[BUFFER_SIZE];
@@ -10,35 +10,31 @@ void setup() {
     Serial.begin(115200);
     delay(2000);
 
-    // begin() after setting
-    // HSPI = CS: 15, CLK: 14, MOSI: 13, MISO: 12 -> default
+    // Begin() with settings for VSPI
     // VSPI = CS:  5, CLK: 18, MOSI: 23, MISO: 19
     slave.setDataMode(SPI_MODE0);
-    slave.begin();
-    // slave.begin(VSPI);   // you can use VSPI like this
+    slave.begin();   // No need to specify VSPI here since it's already provided in the constructor
 
-    // clear buffers
+    // Clear buffers
     memset(spi_slave_tx_buf, 0, BUFFER_SIZE);
     memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
 }
 
 void loop() {
-    // block until the transaction comes from master
+    // Block until the transaction comes from master
     slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
 
-    // if transaction has completed from master,
+    // If transaction has completed from master,
     // available() returns size of results of transaction,
     // and buffer is automatically updated
     while (slave.available()) {
-        // show received data
+        // Show received data
         Serial.print("Command Received: ");
         Serial.println(spi_slave_rx_buf[0]);
         slave.pop();
     }
 
     // Additional delay or processing if needed
-    // ...
-
     delay(1000);  // Delay for a second to easily read the serial data
 }
 
